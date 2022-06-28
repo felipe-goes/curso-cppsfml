8ebd32d3f1e63c3a154fa6166021f0ffa10043b4
Felipe Góes <ogoes.felipe@gmail.com>
2022-06-27 21:35:39 -0300

    Módulo 1: Formas, telas e outras informações.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(1280, 720), "Fundamentos Iniciais");
+  sf::RectangleShape rectangle(sf::Vector2f(100.f, 100.f));
+  rectangle.setFillColor(sf::Color::Green);
+  rectangle.setPosition(
+    (window.getSize().x / 2.f) - (rectangle.getSize().x / 2.f),
+    (window.getSize().y / 2.f) - (rectangle.getSize().y / 2.f));
 
   while (window.isOpen())
   {
@@ -13,8 +18,26 @@ int main()
       {
         window.close();
       }
+
+      auto position = sf::Mouse::getPosition(window);
+
+      if (position.x < window.getSize().x - rectangle.getSize().x &&
+          position.y < window.getSize().y - rectangle.getSize().y)
+      {
+        rectangle.setPosition(position.x, position.y);
+      }
+
+      if (position.x > 600)
+      {
+        rectangle.setFillColor(sf::Color::Red);
+      }
+      else
+      {
+        rectangle.setFillColor(sf::Color::Green);
+      }
+
       window.clear();
-      // window.draw();
+      window.draw(rectangle);
       window.display();
     }
   }
