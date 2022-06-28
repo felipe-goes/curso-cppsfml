0d385572ee3ae1b5802411e445faf23a2553d6d4
Felipe Góes <ogoes.felipe@gmail.com>
2022-06-27 22:10:46 -0300

    Módulo 1: Como adicionar fontes e textos.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(1280, 720), "Fundamentos Iniciais");
-  sf::RectangleShape rectangle(sf::Vector2f(100.f, 100.f));
-  rectangle.setFillColor(sf::Color::Green);
-  rectangle.setPosition(
-    (window.getSize().x / 2.f) - (rectangle.getSize().x / 2.f),
-    (window.getSize().y / 2.f) - (rectangle.getSize().y / 2.f));
+  sf::RenderWindow window(sf::VideoMode(1280, 720), "Textos e Fontes");
+  sf::Font arial, pixel;
+  if (!arial.loadFromFile("../resources/fonts/arial.ttf") ||
+      !pixel.loadFromFile("../resources/fonts/PixelBloated.ttf"))
+  {
+    std::cerr << "Falha ao ler a fonte\n";
+    return EXIT_FAILURE;
+  }
+
+  sf::Text score("Score: 0", arial, 32), life("Life: 3", pixel, 32);
+  score.setPosition(sf::Vector2f(1110.f, 0.f));
+
+  sf::Color rgb(55, 44, 77);
+  score.setFillColor(rgb);
+
+  life.setPosition(sf::Vector2f(10.f, 0.f));
+  life.setFillColor(sf::Color::Red);
+
+  int num{}, num2 = 3;
+  bool pressed = false;
 
   while (window.isOpen())
   {
@@ -19,27 +34,40 @@ int main()
         window.close();
       }
 
-      auto position = sf::Mouse::getPosition(window);
-
-      if (position.x < window.getSize().x - rectangle.getSize().x &&
-          position.y < window.getSize().y - rectangle.getSize().y)
-      {
-        rectangle.setPosition(position.x, position.y);
-      }
-
-      if (position.x > 600)
-      {
-        rectangle.setFillColor(sf::Color::Red);
-      }
-      else
-      {
-        rectangle.setFillColor(sf::Color::Green);
-      }
-
-      window.clear();
-      window.draw(rectangle);
-      window.display();
     }
+
+    if (event.type == event.MouseButtonReleased &&
+        event.type == event.MouseButtonPressed)
+    {
+      pressed = false;
+    }
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Right) && pressed)
+    {
+      --num2;
+      pressed = !pressed;
+      life.setString("Life: " + std::to_string(num2));
+    }
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && !pressed)
+    {
+      ++num;
+      // std::cout << "Você clicou com o botão esquerdo! " << num << "
+      // vezes.\n";
+      score.setString("Score: " + std::to_string(num));
+      pressed = true;
+    }
+
+    if (num2 < 0)
+    {
+      std::cout << "Game Over\n";
+      break;
+    }
+
+    window.clear(sf::Color::White);
+    window.draw(score);
+    window.draw(life);
+    window.display();
   }
 
   return EXIT_SUCCESS;
