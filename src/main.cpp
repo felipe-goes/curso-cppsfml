579855c522de907329fbc3ce7c16e7e3d61c869f
Felipe Góes <ogoes.felipe@gmail.com>
2022-06-27 22:37:30 -0300

    Módulo 1: Utilizando imagens e criando movimentos.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(1280, 720), "Textos e Fontes");
-  sf::Font arial, pixel;
-  if (!arial.loadFromFile("../resources/fonts/arial.ttf") ||
-      !pixel.loadFromFile("../resources/fonts/PixelBloated.ttf"))
-  {
-    std::cerr << "Falha ao ler a fonte\n";
-    return EXIT_FAILURE;
-  }
+  sf::RenderWindow window(sf::VideoMode(1280, 720), "Imagens e Movimento");
+  sf::Texture texture, campo;
+  texture.loadFromFile("../resources/imgs/ball.png");
+  campo.loadFromFile("../resources/imgs/campo.jpg");
 
-  sf::Text score("Score: 0", arial, 32), life("Life: 3", pixel, 32);
-  score.setPosition(sf::Vector2f(1110.f, 0.f));
+  sf::Sprite sprite(texture), fundo(campo);
+  sprite.setPosition(sf::Vector2f(400.f, 200.f));
 
-  sf::Color rgb(55, 44, 77);
-  score.setFillColor(rgb);
+  sf::Vector2u size = texture.getSize();
+  sf::Vector2f velocidade(0.4f, 0.4f);
+  sf::Vector2f pos_text(0.f, -50.f);
 
-  life.setPosition(sf::Vector2f(10.f, 0.f));
-  life.setFillColor(sf::Color::Red);
-
-  int num{}, num2 = 3;
-  bool pressed = false;
+  sf::Font font;
+  font.loadFromFile("../resources/fonts/arial.ttf");
+  sf::Text text("400,200", font);
+  text.setFillColor(sf::Color::Green);
 
   while (window.isOpen())
   {
@@ -33,40 +30,34 @@ int main()
       {
         window.close();
       }
-
     }
 
-    if (event.type == event.MouseButtonReleased &&
-        event.type == event.MouseButtonPressed)
+    if ((sprite.getPosition().x + size.x > window.getSize().x &&
+         velocidade.x > 0) ||
+        (sprite.getPosition().x < 0 && velocidade.x < 0))
     {
-      pressed = false;
+      velocidade.x = -velocidade.x;
+      sprite.setColor(sf::Color::Red);
     }
 
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Right) && pressed)
+    if ((sprite.getPosition().y + size.y > window.getSize().y &&
+         velocidade.y > 0) ||
+        (sprite.getPosition().y < 0 && velocidade.y < 0))
     {
-      --num2;
-      pressed = !pressed;
-      life.setString("Life: " + std::to_string(num2));
+      velocidade.y = -velocidade.y;
+      sprite.setColor(sf::Color::Yellow);
     }
 
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && !pressed)
-    {
-      ++num;
-      // std::cout << "Você clicou com o botão esquerdo! " << num << "
-      // vezes.\n";
-      score.setString("Score: " + std::to_string(num));
-      pressed = true;
-    }
+    sprite.setPosition(sprite.getPosition() + velocidade);
+    text.setPosition(sprite.getPosition() + velocidade + pos_text);
+    text.setString(std::to_string(static_cast<int>(sprite.getPosition().x)) +
+                   "," +
+                   std::to_string(static_cast<int>(sprite.getPosition().y)));
 
-    if (num2 < 0)
-    {
-      std::cout << "Game Over\n";
-      break;
-    }
-
-    window.clear(sf::Color::White);
-    window.draw(score);
-    window.draw(life);
+    window.clear();
+    window.draw(fundo);
+    window.draw(sprite);
+    window.draw(text);
     window.display();
   }
 
