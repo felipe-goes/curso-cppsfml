b9adbe3c621c5fa7b045120c4db5da8bae222f5b
Felipe Góes <ogoes.felipe@gmail.com>
2022-06-29 13:19:41 -0300

    Modulo 2: Limitando escopo dos objetos e surgimento randômico dos mesmos.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
-#include "SFML/Graphics/RectangleShape.hpp"
 #include <SFML/Graphics.hpp>
-#include <memory>
+#include <ctime>
+#include <experimental/random> // se tiver no windows use o <ctime> std::rand()
+#include <iostream>
 
 int main()
 {
+  std::srand(std::time(nullptr));
+
   sf::RenderWindow window(sf::VideoMode(1280, 720), "Minecrap - 0.1");
 
   sf::RectangleShape object;
   object.setSize(sf::Vector2f(100.f, 100.f));
   object.setFillColor(sf::Color(0, 255, 0));
-  object.setPosition(10.f, 10.f);
+
+  float x = static_cast<float>(std::experimental::randint(
+    10, static_cast<int>(window.getSize().x - object.getSize().x)));
+  object.setPosition(x, 10.f);
 
   // Dessa forma, usando a biblioteca <memory> podemos criar ponteiros na Heap
   // sem precisar rodar o delete e atribuir nulo ao ponteiro. Isso se chama
@@ -30,6 +36,14 @@ int main()
         window.close();
       }
     }
+
+    if (object.getPosition().y > window.getSize().y)
+    {
+      x = static_cast<float>(std::experimental::randint(
+        10, static_cast<int>(window.getSize().x - object.getSize().x)));
+      object.setPosition(x, 10.f);
+    }
+    object.move(0.f, 0.4f);
 
     window.clear();
     window.draw(object);
