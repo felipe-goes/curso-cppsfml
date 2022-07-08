0a98fb086b775eaea36e33b502183689934b0859
Felipe Góes <ogoes.felipe@gmail.com>
2022-07-08 16:05:59 -0300

    Template genérico para projeto com SFML.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
+#include <SFML/Graphics.hpp>
+
 int main()
 {
-  return 0;
+  sf::RenderWindow window(sf::VideoMode(1280, 720), "");
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+    }
+
+    window.clear();
+    // window.draw();
+    window.display();
+  }
+
+  return EXIT_SUCCESS;
 }
