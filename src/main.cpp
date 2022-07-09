2363f4eacefcec7eb7177b954830ed5b90699d68
Felipe Góes <ogoes.felipe@gmail.com>
2022-07-09 13:33:44 -0300

    Módulo 5: Desenhando o personagem e fizando ao ground.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,20 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(1280, 720), "");
+  sf::RenderWindow window(sf::VideoMode(1280, 720), "Plataforma 2D",
+                          sf::Style::Close | sf::Style::Titlebar);
+  window.setPosition(sf::Vector2i(0, 0));
+  window.setFramerateLimit(60);
+
+  sf::RectangleShape floor(sf::Vector2f(window.getSize().x, 60.f));
+  floor.setPosition(0.f, window.getSize().y - 60.f);
+  floor.setFillColor(sf::Color::Black);
+
+  sf::Texture texture;
+  texture.loadFromFile("./resources/imgs/afro.png");
+  sf::Sprite sprite(texture, sf::IntRect(0, 0, 43, 82));
+  sprite.setPosition(0, window.getSize().y - sprite.getGlobalBounds().height -
+                          60.f);
 
   while (window.isOpen())
   {
@@ -15,8 +28,18 @@ int main()
       }
     }
 
-    window.clear();
-    // window.draw();
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+      sprite.move(8.f, 0);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+      sprite.move(-8.f, 0);
+    }
+
+    window.clear(sf::Color::Yellow);
+    window.draw(floor);
+    window.draw(sprite);
     window.display();
   }
 
