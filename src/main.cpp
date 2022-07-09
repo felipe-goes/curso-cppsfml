e25adeec29b6d7265fed2230a5247c56fe4cc4c1
Felipe Góes <ogoes.felipe@gmail.com>
2022-07-09 15:11:08 -0300

    Módulo 5: Adicionando classe para movimentos da física aplicados.

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,28 @@
 #include <SFML/Graphics.hpp>
 
-int main()
+class Player
 {
-  sf::RenderWindow window(sf::VideoMode(1280, 720), "Plataforma 2D",
-                          sf::Style::Close | sf::Style::Titlebar);
-  window.setPosition(sf::Vector2i(0, 0));
-  // window.setFramerateLimit(60);
+public:
+  const int ground = 720 - (82 + 100);
+  float dx, dy, frame;
+  bool on_ground;
+  sf::FloatRect rect;
+  sf::Sprite sprite;
 
-  const float height_floor = 100.f, speed = 0.4f;
-  float frame = 0.f;
-
-  sf::Texture texture, floor_texture, bg_texture;
-  texture.loadFromFile("./resources/imgs/afro.png");
-  floor_texture.loadFromFile("./resources/imgs/floormax.jpg");
-  bg_texture.loadFromFile("./resources/imgs/bg.jpg");
-  sf::Sprite sprite(texture, sf::IntRect(0, 0, 43, 82)), floor(floor_texture),
-    bg(bg_texture);
-  sprite.setPosition(0, window.getSize().y -
-                          (sprite.getGlobalBounds().height + height_floor));
-
-  // floor
-  floor.setPosition(0.f, window.getSize().y - height_floor);
-
-  // pulo/gravidade
-  int ground =
-    window.getSize().y - (sprite.getGlobalBounds().height + height_floor);
-  float dx = 0.f, dy = 0.f;
-  bool on_ground = true;
-  sf::FloatRect rect(0, ground, 43, 82);
-  sf::Clock clock;
-
-  while (window.isOpen())
+  Player(sf::Texture &texture)
   {
-    float time = clock.getElapsedTime().asMicroseconds();
-    clock.restart();
-    time = time / 800;
+    sprite.setTexture(texture);
+    sprite.setTextureRect(sf::IntRect(0, 0, 43, 82));
+    rect = sf::FloatRect(0, ground, 43, 82);
+    dx = 0.f;
+    dy = 0.f;
+    frame = 0.f;
+    on_ground = false;
+  }
 
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      if (event.type == sf::Event::Closed)
-      {
-        window.close();
-      }
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-    {
-      dx = speed;
-      // sprite.move(speed * time, 0);
-      frame += 0.5f;
-      if (frame > 6.f)
-      {
-        frame -= 6.f;
-      }
-      // sprite.setTextureRect(sf::IntRect(43 * (int)frame, 0, 43, 82));
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-    {
-      dx = -speed;
-      // sprite.move(-speed * time, 0);
-      frame += 0.5f;
-      if (frame > 6.f)
-      {
-        frame -= 6.f;
-      }
-      // sprite.setTextureRect(sf::IntRect(43 * (int)frame + 43, 0, -43, 82));
-    }
-
+  void update(float time)
+  {
     // PULO/GRAVIDADE
-    // Quando pressionarmos a tecla para cima ele pulará
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-    {
-      if (on_ground)
-      {
-        dy = -speed;
-      }
-    }
-
     // Para usar no setPosition
     rect.left += dx * time;
     rect.top += dy * time;
@@ -97,6 +42,12 @@ int main()
       on_ground = true;
     }
 
+    frame += 0.5f * time;
+    if (frame > 6.f)
+    {
+      frame -= 6.f;
+    }
+
     // Para utilizar os frames(7) para a direita
     if (dx > 0)
     {
@@ -113,11 +64,70 @@ int main()
                                   // armazenados em rect.left e rect.top
     dx = 0; // Para ele movimentar a cada vez que a gente teclar as setas, caso
             // contrário ele ficará andando sequencialmente
+  }
+};
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(1280, 720), "Plataforma 2D",
+                          sf::Style::Close | sf::Style::Titlebar);
+  window.setPosition(sf::Vector2i(0, 0));
+
+  const float height_floor = 100.f, speed = 0.4f;
+
+  sf::Texture texture, floor_texture, bg_texture;
+  texture.loadFromFile("./resources/imgs/afro.png");
+  floor_texture.loadFromFile("./resources/imgs/floormax.jpg");
+  bg_texture.loadFromFile("./resources/imgs/bg.jpg");
+  sf::Sprite floor(floor_texture), bg(bg_texture);
+
+  // floor
+  floor.setPosition(0.f, window.getSize().y - height_floor);
+
+  Player player(texture);
+
+  // pulo/gravidade
+  sf::Clock clock;
+
+  while (window.isOpen())
+  {
+    float time = clock.getElapsedTime().asMicroseconds();
+    clock.restart();
+    time = time / 800;
+
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+      player.dx = speed;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+      player.dx = -speed;
+    }
+
+    // Quando pressionarmos a tecla para cima ele pulará
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+      if (player.on_ground)
+      {
+        player.dy = -speed;
+      }
+    }
+
+    player.update(time);
 
     window.clear(sf::Color::Yellow);
     window.draw(bg);
     window.draw(floor);
-    window.draw(sprite);
+    window.draw(player.sprite);
     window.display();
   }
 
